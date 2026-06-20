493ef16995394464121855a298d9d2dcee31d9d4
Ryan Park <50344020+Parkryan0128@users.noreply.github.com>
2026-06-19 21:54:26 -0700

    Add: eating-bananas - submission-5

---
+++ Data Structures & Algorithms/eating-bananas/submission-5.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int left = 1;
+        int right = 0;
+
+        for (int pile : piles) {
+            right = std::max(right, pile);
+        }
+
+        int ans = right;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            int h_spent = 0;
+            for (int pile : piles) {
+                h_spent += (pile + mid - 1) / mid;
+            }
+
+            if (h_spent <= h) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+};
