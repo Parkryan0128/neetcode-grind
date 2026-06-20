053dd3e9ad60d2863ff0ce5eae9fb87303bc3d4e
Ryan Park <50344020+Parkryan0128@users.noreply.github.com>
2026-06-19 21:35:30 -0700

    Add: sqrtx - submission-13

---
+++ Data Structures & Algorithms/sqrtx/submission-13.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x <= 1) return x;
+
+        int left = 2;
+        int right = x/2;
+
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int sqrt = x / mid;
+
+            if (sqrt == mid) {
+                return mid;
+            } else if (x / mid < mid) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return right;
+    }
+};
