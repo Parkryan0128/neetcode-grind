8bca05a72e78b1609deeabba3d75b88e6965af41
Ryan Park <50344020+Parkryan0128@users.noreply.github.com>
2026-07-07 23:34:14 -0700

    Add: contains-duplicate-ii - submission-3

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        std::unordered_set<int> my_set;
+        int l = 0;
+
+        for (int r=0; r<nums.size(); r++) {
+            if (r - l > k) {
+                my_set.erase(nums[l]);
+                l++;
+            }
+
+            if (my_set.count(nums[r])) {
+                return true;
+            }
+
+            my_set.insert(nums[r]);
+        }
+
+        return false;
+    }
+};
