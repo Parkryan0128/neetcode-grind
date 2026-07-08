690d5b4b179f21e520865563de9d7ea0231bb602
Ryan Park <50344020+Parkryan0128@users.noreply.github.com>
2026-07-07 20:33:16 -0700

    Add: longest-substring-without-duplicates - submission-9

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-9.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        std::unordered_set<char> my_set;
+        int l = 0;
+        int res = 0;
+
+        for (int r = 0; r < s.size(); r++) {
+            while (my_set.count(s[r])) {
+                my_set.erase(s[l]);
+                l++;
+            }
+
+            my_set.insert(s[r]);
+            res = std::max(res, r - l + 1);
+        }
+
+        return res;
+    }
+};
