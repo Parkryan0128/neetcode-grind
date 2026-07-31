5f17741fa337ef52c1204dc362144ac5ad735cd1
Ryan Park <50344020+Parkryan0128@users.noreply.github.com>
2026-07-31 15:48:12 -0700

    Add: longest-consecutive-sequence - submission-4

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-4.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        std::unordered_set<int> my_set;
+
+        for (int item : nums) {
+            my_set.insert(item);
+        }
+
+        int res = 0;
+
+        for (int item : my_set) {
+            if (my_set.find(item-1) == my_set.end()) {
+                int length = 1;
+                while (my_set.find(item + length) != my_set.end()) {
+                    length++;
+                }
+
+                res = max(res, length);
+            }
+        }
+
+        return res;
+    }
+};
