bf5a1b5796039065e70813a5ea40420f440a9331
Ryan Park <50344020+Parkryan0128@users.noreply.github.com>
2026-09-20 11:32:15 -0700

    Add: permutation-string - submission-5

---
+++ Data Structures & Algorithms/permutation-string/submission-5.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s1.size() > s2.size()) {
+            return false;
+        }
+
+        vector<int> count1(26,0);
+        vector<int> count2(26,0);
+
+        for (int i=0; i<s1.size(); i++) {
+            count1[s1[i] - 'a']++;
+            count2[s2[i] - 'a']++;
+        }
+        
+        int l = 0;
+
+        for (int i=s1.size(); i < s2.size(); i++) {
+            if (count1 == count2) return true;
+
+            count2[s2[i]-'a']++;
+            count2[s2[l]-'a']--;
+            l++;
+        }
+
+        return count1 == count2;
+
+    }
+};
