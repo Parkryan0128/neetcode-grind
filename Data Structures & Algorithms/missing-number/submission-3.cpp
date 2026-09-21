ff23da75fb31b93b200a5536d4f09cd581de628b
Ryan Park <50344020+Parkryan0128@users.noreply.github.com>
2026-09-20 22:59:15 -0700

    Add: missing-number - submission-3

---
+++ Data Structures & Algorithms/missing-number/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int res = 0;
+        int sum = 0;
+        for (int i=0; i<=nums.size(); i++) {
+            sum += i;
+        }
+
+        for (int i=0; i<nums.size(); i++) {
+            sum -= nums[i];
+        }
+
+        return sum;
+    }
+};
